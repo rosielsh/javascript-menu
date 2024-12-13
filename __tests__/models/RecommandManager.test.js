b4ad5bf1f151d365eeb88d023a9d444c32055bb2
suhwa <tnghk9611@naver.com>
2024-12-13 15:46:36 +0900

    test: 중복되지 않은 메뉴를 추천하는 테스트 작성

--- __tests__/models/RecommandManager.test.js
+++ __tests__/models/RecommandManager.test.js
@@ -20,4 +20,26 @@ describe("RecommandManager 클래스 테스트", () => {
       expect(cnt).toBeLessThanOrEqual(2);
     });
   });
+
+  test("한 주에 중복되지 않은 메뉴를 추천해야 한다.", () => {
+    const coach = new Coach(["토미", "제임스", "포코"], [["우동"], ["뇨끼"], []]);
+    const manager = new RecommandManager(coach);
+    const recommandResult = manager.recommandMenu();
+
+    const recommandMap = new Map();
+
+    for (let coach = 1; coach < recommandResult[0].length; coach++) {
+      const set = new Set();
+
+      for (let day = 0; day < 5; day++) {
+        set.add(recommandResult[day][coach]);
+      }
+
+      recommandMap.set(coach, set);
+    }
+
+    for (let [key, value] of recommandMap) {
+      expect(value.size).toEqual(5);
+    }
+  });
 });
