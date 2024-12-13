84f801e6af31f16c80f9c69fb44b8a65416ce11a
suhwa <tnghk9611@naver.com>
2024-12-13 15:47:46 +0900

    refactor: 콘솔 삭제

--- src/models/RecommandManager.js
+++ src/models/RecommandManager.js
@@ -44,7 +44,6 @@ class RecommandManager {
       recommandResut.push([RecommandManager.KEY[categoryNumber], ...recommandMenu]);
     }
 
-    console.log(this.#recommandedCategory);
     return recommandResut;
   }
 
